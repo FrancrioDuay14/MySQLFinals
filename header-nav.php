2114be672cf10d9833ab8bf6f3f6039059dcbbbc
FrancrioDuay14 <francduay@gmail.com>
2022-12-11 11:51:30 +0800

    Revision

--- header-nav.php
+++ header-nav.php
@@ -50,13 +50,13 @@
                     <div class="sidebar-sticky pt-3">
                         <ul class="nav flex-column">
                             <li class="nav-item">
-                                <a class="nav-link active" href="dashboard.php">
+                                <a class="nav-link <?php echo ($_SESSION['CURR_PAGE'] == 'dashboard' ? 'active' : '')?>" href="dashboard.php">
                                     <i class="fa fa-tachometer-alt"></i>
                                     Dashboard <span class="sr-only">(current)</span>
                                 </a>
                             </li>
                             <li class="nav-item">
-                                <a class="nav-link" href="products.php">
+                                <a class="nav-link <?php echo ($_SESSION['CURR_PAGE'] == 'products' ? 'active' : '')?>" href="products.php">
                                     <i class="fa-brands fa-shopify"></i>
                                     Products
                                 </a>
